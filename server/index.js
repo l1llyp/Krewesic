8a3d2b7aaa6cfa1032d0c40bd697198370bdd397
Alex McGinnis <alex.z.mcginnis@gmail.com>
2021-10-18 16:54:58 -0500

    setting up postgres

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const port = 1337
 const http = require('http');
 const frontEnd = path.resolve(__dirname, '..', 'client', 'dist')
 
+const {User} = require('../db')
+
 //create the server
 const server = http.createServer(app);
 
