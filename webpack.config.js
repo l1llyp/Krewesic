0a46ff3175de1243f1041cb0c5add08f4d5d9f82
tremitch504 <trevon@students.alcorn.edu>
2021-10-18 14:52:20 -0500

    have the project to render and server, webpack running

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path =  require('path');
+const { NODE_ENV = 'production' } = process.env;
+const isDev = NODE_ENV.includes('dev'); 
+
+module.exports = {
+
+  mode: isDev ? 'development' : 'production',
+  entry: path.resolve(__dirname, 'client', 'src', 'index.jsx'),
+  output: {
+    filename: 'bundles.js',
+    path: path.resolve(__dirname, 'client', 'dist')
+  },
+  watch: isDev,
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg)$/, 
+        use: {
+          loader: 'url-loader?limit=8192'
+        }
+        
+      }
+    ]
+  }
+
+};
