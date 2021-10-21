72b6a2be0573f3b764542fd6328f429795872203
Alex McGinnis <alex.z.mcginnis@gmail.com>
2021-10-21 13:53:05 -0500

    husky

--- client/src/components/Events/Map.jsx
+++ client/src/components/Events/Map.jsx
@@ -1,39 +1,106 @@
-import React from 'react';
-import styled from 'styled-components'
-import {GoogleMap, useJsApiLoader} from '@react-google-maps/api';
+import keys from './keys.js'
+import React, {useState, memo, useEffect} from 'react'
+import { GoogleMap, LoadScript} from '@react-google-maps/api';
+import styled from 'styled-components';
+
+const MapStyles = styled.div`
+  .container {
+    width: '400px',
+  height: '400px'
+  }
+`;
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+//  mapContainerStyle={containerStyle}
+const center = {
+  lat: 30,
+  lng: -90
+};
+
+const Map = () => {
+  return (
+    <MapStyles>
+    <LoadScript
+      googleMapsApiKey={keys.GOOGLE_MAPS_KEY}
+    >
+      <GoogleMap
+      
+      mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+      >
+        
+        <></>
+      </GoogleMap>
+    </LoadScript>
+    </MapStyles>
+  )
+}
+
+export default memo(Map)
+
+
+/**
+ *  import React, {useEffect, useState, useCallback, memo} from 'react';
+import styled from 'styled-components';
+import {GoogleMap, LoadScript} from '@react-google-maps/api';
 //import GoogleMapReact from 'google-map-react';
 //const google = window.google;
 
 const MapStyles = styled.div`
   height: 200px;
   width: 200px;
+  .mapContainer{
+    height: 100px;
+    width: 100px;
+  }
 `;
 
 const Map = ( props) => {
 
   const center = { lat: 30.0, lng: -90.0 }; //coordinates to center map
 
+  // const {isLoaded, loadError} = useJsApiLoader({
+  //   id: 'google-map-script',
+  //   apiKey: 
+  // })
+
+  const [map, setMap] = useState(null);
+
+  const onLoad = useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map);
+  })
+
+  const onUnmount = useCallback(function callback(map) {
+    setMap(null)
+  }, [])
 
 
 
 
-  return (
+
+  return  (
     <MapStyles> 
-      <div>map should b below</div>
-      <GoogleMap
+      <LoadScript
+        googleMapsApiKey='AIzaSyCn9YMdbxwNL3hkpTpVS5mNrUhpyXpg_qg'
+      >
+      <GoogleMap className='mapContainer'
         zoom={12}
         center={center}
-        height={100}
-        width={100}
+        
       />
+      <></>
   
-      
+  </LoadScript>
     </MapStyles>
+  
   )
 }
 
-export default Map
-
-/**
- *  
+export default memo(Map)
  */
--- client/src/components/Events/MapEvents.jsx
+++ client/src/components/Events/MapEvents.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import {TextField, Button} from '@material-ui/core/';
 import axios from 'axios';
 import Map from './Map.jsx'
+
 //import './sample.json';
 
 
@@ -58,5 +59,3 @@ Your can add client_id and optionally client_secret to the end of any valid url 
  * curl https://api.seatgeek.com/2/events?client_id=MYCLIENTID
 curl https://api.seatgeek.com/2/events?client_id=MYCLIENTID&client_secret=MYCLIENTSECRET
  */
-
-//client_id=MjQwMDU0MjB8MTYzNDc0NDg3My44MDkwMTAz&client_secret=3eb3e9c659e45a2d1e07380a0146edf2313490dcbb6c89b377d08f6295208ab1
