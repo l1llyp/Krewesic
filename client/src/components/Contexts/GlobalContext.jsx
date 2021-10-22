d4d44b1dc5a6f4c5ae330cc0ec598d837b4dc5d4
Jacob Young <jyoung3128@gmail.com>
2021-10-21 19:17:46 -0500

    incorporated base profile creation info into user profile/renders info based on user type

--- client/src/components/Contexts/GlobalContext.jsx
+++ client/src/components/Contexts/GlobalContext.jsx
@@ -4,6 +4,12 @@ const GlobalContext = React.createContext({
   name: '',
   type: '',
   bio: '',
+  city: '',
+  artistName: '',
+  favGenre: '',
+  myGenre: '',
+  artistBio: '',
+  favArtist: '',
 });
 
 export default GlobalContext;
