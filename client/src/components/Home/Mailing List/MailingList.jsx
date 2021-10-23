c92c31f6350f0b615c76f23762e1d7b6eb62d1b4
Amelia Neville <muzikgirl12@gmail.com>
2021-10-23 13:17:19 -0500

    adding mailing list component and pushing up artist of day info

--- client/src/components/Home/Mailing List/MailingList.jsx
+++ client/src/components/Home/Mailing List/MailingList.jsx
@@ -1,29 +1,29 @@
 import React, { reactDOM, useContext, useState, useEffect} from 'react';
 import {Router, Route, Link, RouteHandler} from 'react-router';
 import { Redirect } from 'react-router';
-import MailchimpSubscribe from "react-mailchimp-subscribe";
+import MailchimpSubscribe from 'react-mailchimp-subscribe';
 
 
 
 const MailingList = () => {
 
-  const postUrl = `https://gmail.us5.list-manage.com/subscribe/post?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`
+  const postUrl = `https://gmail.us5.list-manage.com/subscribe/post?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
 
 
   return (
     <div className="mc__form-container">
       <MailchimpSubscribe
-                url={postUrl}
-                render={({ subscribe, status, message }) => (
-                    <CustomForm
-                        status={status} 
-                        message={message}
-                        onValidated={formData => subscribe(formData)}
-                    />
-                )}
-            />
+        url={postUrl}
+        render={({ subscribe, status, message }) => (
+          <CustomForm
+            status={status} 
+            message={message}
+            onValidated={formData => subscribe(formData)}
+          />
+        )}
+      />
     </div>
-);
-}
+  );
+};
 
 export default MailingList;
