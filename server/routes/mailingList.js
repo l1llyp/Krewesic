0456b069e127176ebea8920bc78720edcc97d318
Amelia Neville <muzikgirl12@gmail.com>
2021-10-23 16:10:24 -0500

    adding mailing list form component to update users on latest events

--- server/routes/mailingList.js
+++ server/routes/mailingList.js
@@ -9,12 +9,12 @@ const postUrl = `https://gmail.us5.list-manage.com/subscribe/post?u=${process.en
 
 mailingList.get('/mailingList', async(req, res) => {
   try {
-    const { data } = await axios.get(postUrl)
+    const { data } = await axios.get(postUrl);
     console.log(data);
-    res.status(201).send(data)
+    res.status(201).send(data);
   } catch (err) {
     console.error(err);
     res.sendStatus(500);
   }
-})
+});
 module.exports = mailingList;
